a6cf62a2cf1b8d066a8a55bf42d11dd60da8bf13
Himanshi <himanshidosarka@gmail.com>
2024-08-13 10:56:51 +0530

    Blog

---
+++ blogproject/blogapp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+from django.contrib.auth.models import User
+from django.utils import timezone
+from django.urls import reverse
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blogapp-detail', kwargs={'pk': self.pk})
---
+++ blogproject/blogapp/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from . import views
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+
+urlpatterns = [
+    # path('', views.home, name="blogapp-home"),
+    path('about/', views.about, name="blogapp-about"),
+
+    path('', PostListView.as_view(), name="blogapp-home"),
+    path('post-new/', PostCreateView.as_view(), name="blogapp-new"),
+    path('post/<int:pk>/', PostDetailView.as_view(), name="blogapp-detail"),
+    path('post/<int:pk>/update', PostUpdateView.as_view(), name="blogapp-update"),
+    path('post/<int:pk>/delete', PostDeleteView.as_view(), name="blogapp-delete")
+]
